061a4af8e8bb6e0e7eaa04cb0474fa96d2b8e8bd
Yana <yanaegorova69.3@gmail.com>
2021-09-29 20:46:24 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,145 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Drain Stopper',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'This product is constructed of a stain-proof rubber for long-lasting durability.',
+            },
+            {
+                text: 'Has a split-type ring which allows for beaded-chain connection.',
+            },
+            {
+                text: 'This is designed for use with 1-5/8-inch drains in indoor or outdoor kitchen/bathroom sinks as it protects the drain from unwanted debris and clogs.',
+            },
+            {
+                text: 'The white finish of the drain stopper gives a simple, but elegant feel to your sink.',
+            },
+            {
+                text: 'This product is very easy-to-install, with absolutely no tools required.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Power Strip Surge Protector',
+        price: 29.99,
+        image: './img/img2.png',
+        description: [
+            {
+                title: 'Multi-Outlet Surge Protector Power Strip',
+                text: 'Power everything on your desk with a single compact surge-protected extension cord. This reputable power cord offers 12 AC outlets with surge protector for charging your computer, laptop, phone, camera, and more. One charging station for a clutter-free desk.',
+            },
+            {
+                title: 'Power Bar with Phone Line Protection',
+                text: 'Featuring a 1-in-2-out RJ11 telephone or fax protection to ensure open and continuous phone line connections. It also provides a coaxial cable protection to safeguard cable box and satellite connections. The damage-resistant housing protects circuits from fire, impact, and rust.',
+            },
+            {
+                title: 'Safeguard Your Devices and Appliances',
+                text: 'The electric strip has a 3,940-Joule energy rating providing a reliable power extension cord to protect printers, broadband modems, or home theater systems. It secures your electronic devices from overload, short circuit, power spikes, lightning strikes, or fluctuations.',
+            },
+            {
+                title: 'Compact, Heavy Duty, and Space-Saving',
+                text: 'The widely spaced outlets give ample area to fit big plugs and charger bricks. Its slender design makes it ideal for home office, workstations, and game rooms. The durable cord is 8 ft / 2.4 meters long with a 3-prong angle plug convenient for tight spaces.',
+            },
+            {
+                title: 'Reliable Product and Service',
+                text: 'Purchase with confidence as it is backed by a limited lifetime warranty and protected by a dollar 200,000 Connected Equipment Warranty. Check out the full specs: 6,000 volts maximum spike voltage, 15A AC, 125V, and 1875W. Each outlet has a safety slide lock to close unused sockets safeguarding you and your kids.',
+            },
+        ],
+        images: ['./img/img2-1.jpg', './img/img2-2.jpg', './img/img2-3.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Metal Floor Standing',
+        price: 64.90,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'Antique Black brushed copper modern design.',
+            },
+            {
+                text: 'Can be adjusted to a 2 tier, 3 tier or 4 tier Stand.',
+            },
+            {
+                text: 'Crafted from high grade iron for long lasting beauty even under rigorous use.',
+            },
+            {
+                text: 'Great for storing or displaying food or other items.',
+            },
+            {
+                text: 'Wipe clean with soft, damp cloth.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png', './img/img3-4.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Full Quilt Set',
+        price: 98.90,
+        image: './img/img4.png',
+        description: [
+            {
+                text: '100% Cotton.',
+            },
+            {
+                text: 'Set includes quilt, plus two pillow shams (one sham with twin set).',
+            },
+            {
+                text: 'Component dimensions in inches (quantity): 90x90 + 20x26 (2) [all +/-2 inches].',
+            },
+            {
+                text: 'Authentic patchwork construction.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-1.png', './img/img4-3.jpg'],
+    },
+    {
+        id: 'product-5',
+        name: 'Chair',
+        price: 119.98,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Modern accent chair with stylish tufted detailing.',
+            },
+            {
+                text: 'Durable, attractive faux linen polyester upholstery.',
+            },
+            {
+                text: 'Comfortable firm foam padding and elastic suspension system in backrest and seat for exceptional spring.',
+            },
+            {
+                text: 'Strong solid wood legs and framework.',
+            },
+            {
+                text: 'Dimensions measure 22.5"W x 26"D x 33.75"H',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png', './img/img5-4.jpg', './img/img5-5.jpg'],
+    },
+    {
+        id: 'product-6',
+        name: 'Sensor',
+        price: 148.00,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'When used as a third sensor, requires an additional USB 2.0 or higher port.',
+            },
+            {
+                text: 'Sensor tracks constellations of IR LED\'s to translate your movements in VR.',
+            },
+            {
+                text: 'Ideal for most setups and its standard 1/4 20 mount works with most tripods.',
+            },
+            {
+                text: '360 degree and room scale tracking are only supported for 3 sensor configurations.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png'],
+    },
+]
